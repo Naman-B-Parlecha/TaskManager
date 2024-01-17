18aa2cf40b4589c23df7bc814117e32d3f5c3322
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-01-17 23:13:20 +0530

    Test routing

--- Backend/src/main/java/com/taskmanager/backend/BackendApplication.java
+++ Backend/src/main/java/com/taskmanager/backend/BackendApplication.java
@@ -1,13 +1,27 @@
 package com.taskmanager.backend;
 
+import com.taskmanager.backend.model.Task;
+import com.taskmanager.backend.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Autowired
+	private TaskRepository taskRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Task task=new Task();
+		task.setTname("Complete HW");
+		task.setTstatus(false);
+		taskRepository.save(task);
+	}
 }
--- Backend/src/main/java/com/taskmanager/backend/controller/TaskController.java
+++ Backend/src/main/java/com/taskmanager/backend/controller/TaskController.java
@@ -3,15 +3,19 @@ package com.taskmanager.backend.controller;
 import com.taskmanager.backend.model.Task;
 import com.taskmanager.backend.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class TaskController {
     @Autowired
     private TaskRepository taskRepository;
 
+    @GetMapping
     public List<Task> getAllTasks(){
         return taskRepository.findAll();
     }
